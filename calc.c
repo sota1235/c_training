84b1850f8c301f0621378b860a13721ce091acec
sota1235 <sota1235@gmail.com>
2016-01-24 05:45:31 +0900

    modify: print argument

--- calc.c
+++ calc.c
@@ -1,6 +1,16 @@
 #include<stdio.h>
 
+/**
+ * メイン関数
+ */
 int main(int argc, char *argv[]) {
-  printf("引数の数 : %d", argc - 1);
+  int i;
+  printf("引数の数 : %d\n", argc - 1);
+
+
+  for (i=1;i<argc;i++) {
+    printf("%d番目の引数 : %s\n", i, argv[i]);
+  }
+
   return 0;
 }
